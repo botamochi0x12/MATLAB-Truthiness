6f30f3753526ced324fcf97912b21f8e5ac2d75b
botamochi0x12 <35482739+botamochi0x12@users.noreply.github.com>
2022-11-05 10:59:21 +0900

    :bulb: Add documentation for `Logicalness`

--- @Logicalness/Logicalness.m
+++ @Logicalness/Logicalness.m
@@ -1,9 +1,18 @@
 classdef (Abstract) Logicalness < handle
+% Logicalness : The base class for `LazyLogical`, `Truthiness`, `Falsiness` and so on.
+% 
+% This is the base class for some sub-classes containing only one lazily evaluatable value.
+% The sub-classes MUST have the methods such as `not`, `and`, `or`, `andThen`, `orElse`, `unwrap` and `logical`.
+% 
+% See Also: LAZYLOGICAL, TRUTHINESS, FALSINESS
+
     methods(Access = public, Static)
         function clsName = selfClassName()
+        % selfClassName : The class name of the instance.
             clsName = mfilename('class');
         end
         function retval = isLogicalness(val)
+        % isLogicalness : Check if the parameter `val` is an instance of `Logialness`.
             retval = isa(val, 'Logicalness');
         end
     end
