b84d9e22510d9b7c7c6db5678ed1daf5742e8fdc
botamochi0x12 <35482739+botamochi0x12@users.noreply.github.com>
2022-11-16 08:40:57 +0900

    :bulb: Add comment for `Logicalness`

--- @Logicalness/Logicalness.m
+++ @Logicalness/Logicalness.m
@@ -18,13 +18,43 @@ classdef (Abstract) Logicalness < handle
     end
     methods (Abstract)
         % NOTE: The method `Logicalness(val)` doesn't need to define.
+        
+        % not : Perform the logical operation `not` to the value.
+        %
+        % See Also: NOT 
         not(self)
+        
+        % and : Perform the logical operation `and` between the `self`'s value and the passed value.
+        % 
+        % See Also: AND
         and(self, other)
+        
+        % or : Perform the logical operation `or` between the `self`'s value and the passed value.
+        % 
+        % See Also: OR
         or(self, other)
+        
+        % xor : Perform the logical operation `and` between the `self`'s value and the passed value.
+        % 
+        % See Also: XOR
         xor(self, other)
+        
+        % andThen : Perform the logical operation `and` to the `self`'s value and then apply the passed function.
+        % 
+        % See Also: AND
         andThen(self, other)
+        
+        % orElse : Perform the logical operation `or` to the `self`'s value or else apply the passed function.
+        % 
+        % See Also: OR
         orElse(self, other)
+        
+        % unwrap : Unwrap the value of the instance of `self`.
         unwrap(self)
+        
+        % logical : Evaluate the value as either `true` or `false`.
+        % 
+        % See Also: LOGICAL
         logical(self)
     end
 end
